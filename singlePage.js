aa9c33896a293f244528e933e55f65109cd714b6
bharr102 <bharr102@mtroyal.ca>
2023-11-14 15:07:03 -0700

    changed javascript so it works with the dom tree

--- singlePage.js
+++ singlePage.js
@@ -3,23 +3,50 @@ const genre = JSON.parse(genres);
 const song = JSON.parse(songs);
 
 function selectArtist(){
-    document.write("<select>");
+    //getting into the main file
+    const main = document.querySelector("main");
+
+    //creating form 
+    const artistGenreForm = document.createElement("form");
+    //creating a select elemeent for all the artists 
+    const select = document.createElement("select");
+
         for(a of artist){
-            document.write(`<option value ='${artist.id}>${a.name}</option>`);
+            // storing the current artists name as a text node
+            const artistName = document.createTextNode(`${a.name}`);
+           // creating option elemenet
+            const option = document.createElement("option");
+            //adding the artist name as a text node inbetween the option tags 
+            option.appendChild(artistName);
+            //adding this artists name as one of the options in the select tag
+            select.appendChild(option);
+            
         }
-    document.write("</select>");
+        //adding the select into the form tag
+        artistGenreForm.appendChild(select);
+        //adding the form tag into the main html tag
+        main.append(artistGenreForm);
+
+   
 }
 
 function selectGenre(){
-    document.write("<select id = 'genreSelect'>");
+    
+  const main = document.querySelector("main");
+  const select = document.createElement("select");
+  const form = document.querySelector("form");
+  
+  for (g of genre){
+    const genreName = document.createTextNode(`${g.name}`);
+    const option = document.createElement("option");
+    option.appendChild(genreName);
 
-    for (g of genre){
-       document.write(`<option value = '${g.id}'>${g.name}</option>`)
-    }
-       
-    document.write("</select");
+    select.appendChild(option);
+  }
+  form.appendChild(select);
+  main.appendChild(form);
+
 }
 
 selectArtist();
 selectGenre();
-console.log()
