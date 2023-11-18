89ae60d040df69c6db6880afbd225b97ea9900e5
bharr102 <bharr102@mtroyal.ca>
2023-11-17 23:51:33 -0700

    more code

--- singlePage.js
+++ singlePage.js
@@ -38,6 +38,9 @@ function selectArtist(){
             const option = document.createElement("option");
             //adding the artist name as a text node inbetween the option tags 
             option.appendChild(artistName);
+
+            //adding value to artist option
+            option.setAttribute("value",`${a.id}`);
             //adding this artists name as one of the options in the select tag
             select.appendChild(option);
             
@@ -49,6 +52,9 @@ function selectArtist(){
    
 }
 
+/**
+ * function for filling out the select with options of all genres
+ */
 function selectGenre(){
   
   const select = document.querySelector("#genreSelect");
@@ -70,41 +76,82 @@ function selectGenre(){
 
 }
 
-function populateTable(){
-
-}
-
-//adding handler for filter button 
-const clearBtn = document.querySelector("#filter");
-const songSelect = document.querySelector("#songSelect");
-clearBtn.addEventListener("click",()=>{
-  // getting all the radio buttons 
-  const buttons = document.querySelector("input");
-
-  // checking for index of radio button to see which one is chosen 
-  const foundIndex = 0;
-  for (const i =0; i < buttons.length; i++){
-    if (butt.checked){
-      foundIndex = i;
-    }
-    const button = buttons[i];
-
-    // filtering process depending on the selected radio button
-    if (button.value == "song"){
-
-    }
-    else if (button.value== "genre"){
-  
-    }
-    else if (button.value = "artist"){
-
-    }
-    else{
-
+/**
+ * function for finding song selected
+*/
+function searchArtist(id){
+  const songList = [];
+  for (s of song){
+    if (id == s.id){
+      songList.push(s);
     }
   }
-  alert(`${songSelect.value}`);
-});
+  return songList;
+}
+
+/**
+ * function  for finding specific artist selected
+ */
+function searchArtists(id){
+  const songList = []; 
+  for (s of song){
+    if (id == s.artist.id){
+      songList.push(s);
+    }
+  }
+  return songList;
+}
+
+/**
+ * function  for finding specific genre selected
+ */
+function searchGenres(id){
+  const songList = []; 
+  for (s of song){
+    if (id == s.genre.id){
+      songList.push(s);
+    }
+  }
+
+  return songList;
+}
+
+function populateTable(songList){
+  const tbody = document.querySelector("tbody");
+
+  for(s of songList){
+    const row = document.createElement("tr");
+    const titleRow = document.createElement("td");
+    const artistRow = document.createElement("td");
+    const yearRow = document.createElement("td");
+    const genreRow = document.createElement("td");
+    const popularityRow = document.createElement("td");
+
+    //creating text nodes for each td
+    const t = document.createTextNode(s.title);
+    console.log(t);
+    const a = document.createTextNode(s.artist.name);
+    const y = document.createTextNode(s.year);
+    const g = document.createTextNode(s.genre.name);
+    const p = document.createTextNode(s.popularity);
+
+    titleRow.appendChild(t);
+    artistRow.appendChild(a);
+    yearRow.appendChild(y);
+    genreRow.appendChild(g);
+    popularityRow.appendChild(p);
+
+    row.appendChild(titleRow);
+    row.appendChild(artistRow);
+    row.appendChild(yearRow);
+    row.appendChild(genreRow);
+    row.appendChild(popularityRow);
+
+    tbody.appendChild(row);
+  }
+}
+
+
 
 
 
@@ -112,3 +159,43 @@ clearBtn.addEventListener("click",()=>{
 selectArtist();
 selectGenre();
 selectSong();
+
+//adding handler for filter button 
+const clearBtn = document.querySelector("#filter");
+const songSelect = document.querySelector("#songSelect");
+clearBtn.addEventListener("click",()=>{
+  // getting all the radio buttons 
+  const buttons = document.querySelectorAll("input");
+
+  // checking for index of radio button to see which one is chosen 
+  let foundIndex = 0;
+  for (let i =0; i < buttons.length; i++){
+    if (buttons[i].checked){
+      foundIndex = i;
+    }
+    const button = buttons[foundIndex];
+
+    // filtering process depending on the selected radio button
+    
+    // variable for songs that will be displayed on table
+    let songList = [];
+    if (button.value == "song"){
+      const songChoice = document.querySelector("#songSelect");
+      songList = searchGenres(songChoice.value);
+      populateTable(songList);
+    }
+    else if (button.value== "genre"){
+      const genreChoice = document.querySelector("#genreSelect");
+      songList = searchGenres(genreChoice.value);
+      populateTable(songList);
+    }
+    else if (button.value = "artist"){
+      const artistChoice = document.querySelector("#artistSelect");
+      songList = searchArtists(artistChoice.value);
+      populateTable(songList);
+    }
+    else{
+
+    }
+  }
+});
